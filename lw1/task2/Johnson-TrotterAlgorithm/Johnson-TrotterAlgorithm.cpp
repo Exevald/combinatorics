c5e579b556bd88c4faa6e24fbc580c26becfea3b
Konstantin <64555204+Exevald@users.noreply.github.com>
2023-01-17 22:25:54 +0300

    Реализовал алгоритм Джонсона-Троттера

--- lw1/task2/Johnson-TrotterAlgorithm/Johnson-TrotterAlgorithm.cpp
+++ lw1/task2/Johnson-TrotterAlgorithm/Johnson-TrotterAlgorithm.cpp
@@ -3,38 +3,62 @@
 #include <vector>
 #include <fstream>
 #include <boost/timer.hpp>
-#define N 10
+#define count 10
 
-int main()
+void TimeCount(int initialCount, double duration, std::ofstream& output)
 {
-	setlocale(LC_ALL, "rus");
+	long double dur = duration;
+	for (int i = initialCount + 1; i <= 15; i++)
+	{
+		dur *= i;
+	}
+	dur /= 3600;
+	output << "N = 15: " << dur << " hours" << std::endl;
 
+	for (int i = 16; i <= 20; i++)
+	{
+		dur *= i;
+	}
+	dur /= 24;
+	output << "N = 20: " << dur << " days" << std::endl;
+
+	for (int i = 21; i <= 50; i++)
+	{
+		dur *= i;
+	}
+	dur /= 365;
+	output << "N = 50: " << dur << " years" << std::endl;
+
+	for (int i = 51; i <= 100; i++)
+	{
+		dur *= i;
+	}
+	output << "N = 100: " << dur << " years" << std::endl;
+}
+
+double GeneratePermutations(std::vector<int> cur, std::vector<int> rev, std::vector<int> dir)
+{
+	int m = 0;
+	int n = 0;
 	boost::timer t;
 	t.restart();
 
-	std::vector<int> cur;
-	std::vector<int> rev;
-	std::vector<int> dir;
-	cur.resize(N + 2);
-	rev.resize(N + 2);
-	dir.resize(N + 2);
+	cur.resize(count + 2);
+	rev.resize(count + 2);
+	dir.resize(count + 2);
 
-	for (int i = 1; i <= N; i++)
+	for (int i = 1; i <= count; i++)
 	{
 		cur[i] = i;
 		rev[i] = i;
 		dir[i] = -1;
 	}
-
-	cur[0] = N + 1;
-	cur[N + 1] = N + 1;
+	cur[0] = count + 1;
+	cur[count + 1] = count + 1;
 	dir[0] = 0;
-
-	int m = 0;
-	int n = 0;
 	while (m != 1)
 	{
-		m = N;
+		m = count;
 		while ((cur[rev[m] + dir[m]] > m) && (m > 1))
 		{
 			dir[m] = -dir[m];
@@ -44,7 +68,25 @@ int main()
 		std::swap(rev[cur[rev[m]]], rev[cur[rev[m] + dir[m]]]);
 	}
 	double duration = t.elapsed();
+	return duration;
+}
 
-	std::cout << std::endl << duration << std::endl;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	double duration;
+	std::vector<int> cur;
+	std::vector<int> rev;
+	std::vector<int> dir;
+	std::ofstream outputFile("results.txt");
+
+	if (!outputFile.is_open())
+	{
+		puts("Не удалось открыть файл для записи");
+		exit(1);
+	}
+	duration = GeneratePermutations(cur, rev, dir);
+	outputFile << "N = " << count << ": " << duration << " seconds" << std::endl;
+	TimeCount(count, duration, outputFile);
 	return 0;
 }
